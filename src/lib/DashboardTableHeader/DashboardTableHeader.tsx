994932eefae557db22ece34a4c1fece4ac538919
Elsa Tamara <77706500+elsatmr@users.noreply.github.com>
2022-04-22 10:54:51 +0700

    very basic ui for dropdowns

--- src/lib/DashboardTableHeader/DashboardTableHeader.tsx
+++ src/lib/DashboardTableHeader/DashboardTableHeader.tsx
@@ -1,13 +1,29 @@
-import { Box, Button, Paper, Tab, Tabs } from "@mui/material";
+import {
+  Box,
+  Button,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Paper,
+  Tab,
+  Tabs,
+  TextField,
+} from "@mui/material";
 import { CalendarPicker } from "@mui/x-date-pickers";
 import React from "react";
 import { useAppSelector } from "../../redux/hooks";
 import ViewsDatePicker from "../CalendarPicker/CalendarPicker";
 import styles from "./DashboardTableHeader.module.css";
 import CachedOutlinedIcon from "@mui/icons-material/CachedOutlined";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CheckBoxOutlineBlankOutlinedIcon from "@mui/icons-material/CheckBoxOutlineBlankOutlined";
+import CheckBoxOutlinedIcon from "@mui/icons-material/CheckBoxOutlined";
+import AddIcon from "@mui/icons-material/Add";
 
 interface FilterProps {
-  filters: Set<string>;
+  filters: Set<string> | Array<string>;
   header: string;
 }
 
@@ -26,24 +42,98 @@ const SavedFilterDropdown = () => {
     <form>
       <select>
         <option>Saved Filter</option>
+        <option>
+          <Button>
+            <AddIcon />
+            Create new
+          </Button>
+        </option>
       </select>
     </form>
+  );
+};
+
+const CreateNewButton = () => {
+  return (
+    <Button>
+      <AddIcon />
+      Create new
+    </Button>
   );
 };
 
 const FilterDropdown = ({ filters, header }: FilterProps) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [endArrow, setEndArrow] = React.useState(<KeyboardArrowDownIcon />);
+  const handleDropdownClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setEndArrow(<KeyboardArrowUpIcon />);
+    setAnchorEl(event.currentTarget);
+  };
+  const handleDropdownClose = () => {
+    setAnchorEl(null);
+    setEndArrow(<KeyboardArrowDownIcon />);
+  };
+  const open = Boolean(anchorEl);
+  let placeholder: string = "Type " + header + "...";
   return (
-    <form>
-      <select id={header} onChange={() => {}}>
-        <option>{header}</option>
+    <>
+      <Button
+        variant="contained"
+        endIcon={endArrow}
+        onClick={(e) => {
+          handleDropdownClick(e);
+        }}
+        disableElevation
+        sx={{
+          width: "176px",
+          height: "34px",
+          backgroundColor: "#F5F6F7",
+          color: "black",
+          "&.MuiButtonBase-root:hover": {
+            bgcolor: "transparent",
+          },
+        }}
+      >
+        {header}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+        onClose={handleDropdownClose}
+      >
+        <MenuItem
+          sx={{
+            "&.MuiButtonBase-root:hover": {
+              bgcolor: "transparent",
+            },
+          }}
+        >
+          <TextField placeholder={placeholder} size="small" />
+        </MenuItem>
         {[...filters].map((elem) => {
-          return <option>{elem}</option>;
+          return (
+            <MenuItem sx={{ width: "176px" }}>
+              <ListItemIcon>
+                <CheckBoxOutlineBlankOutlinedIcon />
+              </ListItemIcon>
+              <ListItemText>{elem}</ListItemText>
+            </MenuItem>
+          );
         })}
-      </select>
-    </form>
+        {header === "Saved Filter" ? <CreateNewButton /> : <></>}
+        <MenuItem>
+          <Button id="applyFilter" onClick={handleDropdownClose}>
+            Apply
+          </Button>
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
-
 const MoreFilterPicker = () => {
   const alertListState = useAppSelector((state) => state.alertList.alerts);
   const locations: Set<string> = new Set();
@@ -51,6 +141,8 @@ const MoreFilterPicker = () => {
   const agents: Set<string> = new Set();
   const alertQueues: Set<string> = new Set();
   const alertStatus: Set<string> = new Set();
+  //TODO: FETCH SAVED FILTER
+  const savedFilter = ["Apple", "Oranges", "Boba"];
   alertListState.forEach((elem) => {
     locations.add(elem.location);
     regulators.add(elem.regulator);
@@ -69,7 +161,8 @@ const MoreFilterPicker = () => {
           <FilterDropdown filters={agents} header={"Agent"} />
           <FilterDropdown filters={alertQueues} header={"Alert Queue"} />
           <AlertStatusDropdown />
-          <SavedFilterDropdown />
+          <FilterDropdown filters={savedFilter} header={"Saved Filter"} />
+
           <Button>
             <CachedOutlinedIcon fontSize="small" sx={{ mr: 0.5 }} />
             Reset All Filters
